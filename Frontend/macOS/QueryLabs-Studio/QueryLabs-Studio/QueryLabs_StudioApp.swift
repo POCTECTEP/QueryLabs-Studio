abe078918fb78f719d9cebc49602f6ecebcbd5da
POCTECTEP <artstanislavsky2020@gmail.com>
2026-09-25 03:31:42 +0300

    chore(macOS): initialize Xcode project

---
+++ Frontend/macOS/QueryLabs-Studio/QueryLabs-Studio/QueryLabs_StudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  QueryLabs_StudioApp.swift
+//  QueryLabs-Studio
+//
+//  Created by Артём on 25.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct QueryLabs_StudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
